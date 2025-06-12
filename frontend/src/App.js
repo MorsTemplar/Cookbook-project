cccd91de8b30ab524d4e564e1ec995751c916e8d
MorsTemplar <msaqlain8488@gmail.com>
2025-06-12 18:59:38 +0500

    did basic routing in app.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginRegister from './pages/LoginRegister';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<LoginRegister />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
