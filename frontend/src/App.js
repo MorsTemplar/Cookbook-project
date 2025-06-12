65bf7addb2804c64f3cb8e2d280c1112c2f27637
MorsTemplar <msaqlain8488@gmail.com>
2025-06-12 19:19:51 +0500

    imported app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginRegister from './pages/LoginRegister';
 import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
