8570137d209e6e32ef79526cc0067d1d4dd3b429
MorsTemplar <msaqlain8488@gmail.com>
2025-06-12 18:41:19 +0500

    comments field worked on

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -9,4 +9,21 @@ const recipeSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 }, { timestamps: true });
 
+comments: [
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+],
+
 module.exports = mongoose.model('Recipe', recipeSchema);
